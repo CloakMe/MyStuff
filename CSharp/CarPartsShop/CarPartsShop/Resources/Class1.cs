be223ae336a73d7e52d152b1db4be87bc1b7fa13
ka <angelow@math.bas.bg>
2019-07-27 10:58:03 +0200

    add Resources/Class1.cs

---
+++ CSharp/CarPartsShop/CarPartsShop/Resources/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPartsShop.Resources
+{
+    class Class1
+    {
+//Host: sql7.freesqldatabase.com
+//Database name: sql7299547
+//Database user: sql7299547
+//Database password: pAxwgTkKaS
+//Port number: 3306
+
+//same rearMirror for renault and nissan
+//different breaks for renault and nissan
+//but both entries have the same sql input
+//To distinguish b/n different car parts please input unique car part name.
+    }
+}
