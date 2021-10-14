815efac741927761c94d7debf7f4e4ea2c2a804f
pythonuser <pythonuser@abv.bg>
2021-10-14 13:00:02 +0300

    add nfl statistics

---
+++ nfl/nflStats/nflShootOuts/Matchup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nflShootOuts
+{
+    public class Matchup
+    {        
+        public string awayTeam;
+        public int awayTeamOffensiveRankFPTS;
+        public int awayTeamDefensiveRank;
+
+        public string homeTeam;
+        public int homeTeamOffensiveRankFPTS;
+        public int homeTeamDefensiveRank;
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendFormat("{0,2} vs {1,2} ", awayTeamOffensiveRankFPTS, homeTeamDefensiveRank);
+            result.Append(awayTeam);
+            for(int i=awayTeam.Count(); i< 14; i++)
+                result.Append(" ");
+
+            result.AppendFormat("VS {0} ", homeTeam);
+            for (int i = homeTeam.Count(); i < 14; i++)
+                result.Append(" ");
+
+            result.AppendFormat("{0,2} vs {1,2} ", homeTeamOffensiveRankFPTS, awayTeamDefensiveRank);
+
+            if (awayTeamOffensiveRankFPTS + 9 < homeTeamDefensiveRank && homeTeamOffensiveRankFPTS + 5 < awayTeamDefensiveRank)
+                result.AppendFormat(" SHOOT OUT!");
+            else if (awayTeamOffensiveRankFPTS + 6 < homeTeamDefensiveRank && homeTeamOffensiveRankFPTS + 3 < awayTeamDefensiveRank)
+                result.AppendFormat(" ShOoT OuT!");
+            else if (awayTeamOffensiveRankFPTS + 3 < homeTeamDefensiveRank && homeTeamOffensiveRankFPTS + 1 < awayTeamDefensiveRank)
+                result.AppendFormat(" shoot out!");
+            else if (awayTeamOffensiveRankFPTS - 2 > homeTeamDefensiveRank && homeTeamOffensiveRankFPTS - 3 > awayTeamDefensiveRank)
+                result.AppendFormat(" stingy!");
+            else if (awayTeamOffensiveRankFPTS - 4 > homeTeamDefensiveRank && homeTeamOffensiveRankFPTS - 6 > awayTeamDefensiveRank)
+                result.AppendFormat(" StInGy!");
+            else if (awayTeamOffensiveRankFPTS - 6 > homeTeamDefensiveRank && homeTeamOffensiveRankFPTS - 9 > awayTeamDefensiveRank)
+                result.AppendFormat(" STINGY!");
+            return result.ToString();
+        }
+    }
+}
---
+++ nfl/nflStats/nflShootOuts/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nflShootOuts
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int subCounter = 0;
+            string[] OffensiveRankingFPTS = new string[32];
+            // Read the file and display it line by line.  
+            foreach (string line in System.IO.File.ReadLines(@"F:\who\nfl\OffensiveRankingFPTS.txt"))
+            {                
+                if (line.Count() > 12)
+                {
+                    OffensiveRankingFPTS[subCounter] = line;
+                    subCounter++;
+                }
+                counter++;
+            }
+            System.Console.WriteLine("There were {0} lines.", counter);
+
+            string[] DefensiveRanking = new string[32];
+            subCounter = 0;
+            foreach (string line in System.IO.File.ReadLines(@"F:\who\nfl\DefensiveRanking.txt"))
+            {
+                if (line.Count() > 12)
+                {
+                    DefensiveRanking[subCounter] = line;
+                    subCounter++;
+                }
+                counter++;
+            }
+            System.Console.WriteLine("There were {0} lines.", counter);
+
+            while (true)
+            {
+                System.Console.Write(">");
+                string command = System.Console.ReadLine();
+                if (command.Contains("exit"))
+                {
+                    break;
+                }
+
+                CreateMatchupTable(OffensiveRankingFPTS, DefensiveRanking, command);
+            }
+            
+        }
+
+        static void CreateMatchupTable(string[] OffensiveRankingFPTS, string[] DefensiveRanking, string fileNumber)
+        {
+            int counter = 0;
+            int subCounter = 0;
+            bool flag = false;
+            Matchup matchup = new Matchup();
+            List<Matchup> matchups = new List<Matchup>();
+            string fileName = @"F:\who\nfl\MatchupWeek" + fileNumber + ".txt";
+            foreach (string line in System.IO.File.ReadLines(fileName))
+            {
+                if (subCounter == 1 && !line.Contains("Sunday") && !line.Contains("Monday") && !line.Contains("Thursday"))
+                {//next line after matchup
+                    matchup.awayTeam = line.Replace("[\n\r]", "").Trim();
+                    matchup.awayTeamOffensiveRankFPTS = 1 + Array.FindIndex(OffensiveRankingFPTS, (x) => x.Contains(matchup.awayTeam));
+                    matchup.awayTeamDefensiveRank = 1 + Array.FindIndex(DefensiveRanking, (x) => x.Contains(matchup.awayTeam));
+                    subCounter++;
+                    flag = true;
+                    continue;
+                }
+
+                if (subCounter == 2 && flag)
+                {//next next line after matchup
+                    matchup.homeTeam = line.Replace("[\n\r]", "").Trim();
+                    matchup.homeTeamOffensiveRankFPTS = 1 + Array.FindIndex(OffensiveRankingFPTS, (x) => x.Contains(matchup.homeTeam));
+                    matchup.homeTeamDefensiveRank = 1 + Array.FindIndex(DefensiveRanking, (x) => x.Contains(matchup.homeTeam));
+                    subCounter++;
+                    matchups.Add(matchup);
+                    System.Console.WriteLine(matchup.ToString());
+                    matchup = new Matchup();
+                    flag = false;
+                    continue;
+                }
+
+                if (line.Contains("matchup") || line.Contains("Tickets") || line[0] == '\t')
+                {
+                    subCounter = 0;
+                    subCounter++;
+                }
+                else
+                {
+                    subCounter++;
+                }
+
+                counter++;
+            }
+            System.Console.WriteLine("There were {0} lines.", counter);
+        }
+    }
+}
